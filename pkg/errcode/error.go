b4bc2c51886317d459856406aa4c5ae2e959ced4
邱佳飞 <253896514@qq.com>
2020-08-20 16:28:43 +0800

    errcode

--- pkg/errcode/error.go
+++ pkg/errcode/error.go
@@ -3,11 +3,13 @@ package errcode
 const (
 	OK = iota
 	Bad
+	MissArgument
 	UserNotFound
 )
 
 var ErrMsg = map[int]string {
 	OK: "请求成功",
 	Bad: "请求失败",
+	MissArgument: "缺少必要参数",
 	UserNotFound: "用户不存在",
 }
