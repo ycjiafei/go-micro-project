51acf5bda30bf967554ffb1e3f0e503b9a2dead1
邱佳飞 <253896514@qq.com>
2020-08-21 11:56:34 +0800

    自动新建表

--- database/mysql.go
+++ database/mysql.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/ycjiafei/go-micro-project/database/structs"
 	"os"
 )
 
+/**
+搞成单例更好用
+ */
 func NewMysqlConn() (*gorm.DB, error) {
 	db, err := gorm.Open("mysql", fmt.Sprintf(
 		"%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
@@ -15,5 +19,16 @@ func NewMysqlConn() (*gorm.DB, error) {
 		os.Getenv("GO_MICRO_DB_HOST"),
 		os.Getenv("GO_MICRO_DB_NAME"),
 	))
+	autoMigrateTables(db)
 	return db, err
+}
+
+/**
+连接时候自动建表
+ */
+func autoMigrateTables(db *gorm.DB) {
+	tables := []interface{}{
+		&structs.UserInfo{},
+	}
+	db.AutoMigrate(tables...)
 }
